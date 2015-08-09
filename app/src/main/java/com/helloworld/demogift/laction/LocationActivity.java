917f6af175e3d2545d9d83a3a4191c7149dc660f
岳大禹 <yuedayu13@gmail.com>
2015-08-10 05:30:03 +0800

    增加地图

--- app/src/main/java/com/helloworld/demogift/laction/LocationActivity.java
+++ app/src/main/java/com/helloworld/demogift/laction/LocationActivity.java
@@ -1,55 +1,114 @@
 package com.helloworld.demogift.laction;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.widget.ListPopupWindow;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.model.LatLngBounds;
 import com.helloworld.demogift.R;
 
 /**
  * Created by YueXy on 7/30/2015.
  */
-public class LocationActivity extends Activity
-{
-	MapView mMapView = null;
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		SDKInitializer.initialize(getApplicationContext());
-		setContentView(R.layout.location_layout);
-		mMapView = (MapView) findViewById(R.id.bmapView);
-		init();
-	}
+public class LocationActivity extends Activity {
+    MapView mMapView = null;
+    BaiduMap mBaiduMap;
+    private EditText etTest = null;
+    private ListPopupWindow lpw;
+    private String[] list;
 
-	private void init()
-	{
-		//
-	}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.location_layout);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        LatLng point = new LatLng(40.015391, 116.332535);
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.mipmap.demo_map_position_1);
+        OverlayOptions option = new MarkerOptions()
+                .position(point)
+                .icon(bitmap);
+        mBaiduMap.addOverlay(option);
+        etTest = (EditText)findViewById(R.id.search_edit);
+        etTest.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int DRAWABLE_RIGHT = 2;
 
-	@Override
-	public void finish()
-	{
-		super.finish();
-		overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
-	}
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if (motionEvent.getX() >= (view.getWidth() - ((EditText) view)
+                            .getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        lpw.show();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		//在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-		mMapView.onDestroy();
-	}
-	@Override
-	protected void onResume() {
-		super.onResume();
-		//在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-		mMapView.onResume();
-	}
-	@Override
-	protected void onPause() {
-		super.onPause();
-		//在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-		mMapView.onPause();
-	}
+        list = new String[] { "紫荆公寓", "宿舍", "食堂", "超市" };
+        lpw = new ListPopupWindow(this);
+//        lpw.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        lpw.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list));
+        lpw.setAnchorView(etTest);
+        lpw.setModal(true);
+        lpw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = list[i];
+                etTest.setText(item);
+                lpw.dismiss();
+            }
+        });
+        init();
+    }
+
+    private void init() {
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
 }
